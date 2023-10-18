11aa164f532f86a5efd1d61b5863af2dc025efd0
OleksandrKucherenko <OleksandrKucherenko@users.noreply.github.com>
2023-10-18 10:21:14 +0000

    deploy: e989705926de6ad0c6b8eea09773989ccf049844

--- e-bash [specfiles].94663abb81bd2331/index.js
+++ e-bash [specfiles].94663abb81bd2331/index.js
@@ -1,11 +1,10 @@
 var data = {files:[
-{"link":"version-up.sh.edc7c52b.html","title":"version-up.sh","summary_name":"[...]/.scripts/version-up.sh","covered_class":"lineNoCov","covered":"0.0","covered_lines":"0","uncovered_lines":"255","total_lines" : "255"},
-{"link":"_arguments.sh.58a082a5.html","title":"_arguments.sh","summary_name":"[...]/.scripts/_arguments.sh","covered_class":"lineCov","covered":"79.2","covered_lines":"95","uncovered_lines":"25","total_lines" : "120"},
 {"link":"_dependencies.sh.7b9878ab.html","title":"_dependencies.sh","summary_name":"[...]/.scripts/_dependencies.sh","covered_class":"lineCov","covered":"96.9","covered_lines":"62","uncovered_lines":"2","total_lines" : "64"},
 {"link":"_colors.sh.9e4c4a46.html","title":"_colors.sh","summary_name":"[...]/.scripts/_colors.sh","covered_class":"lineCov","covered":"100.0","covered_lines":"18","uncovered_lines":"0","total_lines" : "18"},
 {"link":"_logger.sh.c5dd7ff7.html","title":"_logger.sh","summary_name":"[...]/.scripts/_logger.sh","covered_class":"lineCov","covered":"93.3","covered_lines":"14","uncovered_lines":"1","total_lines" : "15"},
-{"link":"_commons.sh.86725ccf.html","title":"_commons.sh","summary_name":"[...]/.scripts/_commons.sh","covered_class":"lineNoCov","covered":"8.3","covered_lines":"7","uncovered_lines":"77","total_lines" : "84"},
+{"link":"_arguments.sh.58a082a5.html","title":"_arguments.sh","summary_name":"[...]/.scripts/_arguments.sh","covered_class":"lineCov","covered":"79.2","covered_lines":"95","uncovered_lines":"25","total_lines" : "120"},
+{"link":"_commons.sh.86725ccf.html","title":"_commons.sh","summary_name":"[...]/.scripts/_commons.sh","covered_class":"linePartCov","covered":"46.5","covered_lines":"40","uncovered_lines":"46","total_lines" : "86"},
 ]};
 var percent_low = 25;var percent_high = 75;
-var header = { "command" : "shellspec spec", "date" : "2023-10-07 16:49:08", "instrumented" : 556, "covered" : 196,};
+var header = { "command" : "shellspec spec", "date" : "2023-10-18 10:21:11", "instrumented" : 303, "covered" : 229,};
 var merged_data = [];
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var data = {files:[
-{"link":"e-bash%20%5Bspecfiles%5D.94663abb81bd2331/index.html","title":"e-bash [specfiles]","summary_name":"e-bash [specfiles]","covered_class":"linePartCov","covered":"35.3","covered_lines":"196","uncovered_lines":"360","total_lines" : "556"},
+{"link":"e-bash%20%5Bspecfiles%5D.94663abb81bd2331/index.html","title":"e-bash [specfiles]","summary_name":"e-bash [specfiles]","covered_class":"lineCov","covered":"75.6","covered_lines":"229","uncovered_lines":"74","total_lines" : "303"},
 ], merged_files:[]};
 var percent_low = 25;var percent_high = 75;
-var header = { "command" : "shellspec spec", "date" : "2023-10-07 16:49:08", "instrumented" : 556, "covered" : 196,};
+var header = { "command" : "shellspec spec", "date" : "2023-10-18 10:21:11", "instrumented" : 303, "covered" : 229,};
