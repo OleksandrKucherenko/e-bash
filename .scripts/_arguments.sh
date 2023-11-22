77777f28a9a7ee364e094b836a96f6d877ad333e
Oleksandr Kucherenko <o.kucherenko2@klarna.com>
2023-11-22 19:13:25 +0100

    new UI controls

--- .scripts/_arguments.sh
+++ .scripts/_arguments.sh
@@ -16,200 +16,200 @@ source "$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/_commons.sh"
 # array of script arguments cleaned from flags (e.g. --help)
 if [ -z "$ARGS_NO_FLAGS" ]; then export ARGS_NO_FLAGS=(); fi
 function parse:exclude_flags_from_args() {
-	local args=("$@")
+  local args=("$@")
 
-	# remove all flags from call
-	for i in "${!args[@]}"; do
-		if [[ ${args[i]} == --* ]]; then unset 'args[i]'; fi
-	done
+  # remove all flags from call
+  for i in "${!args[@]}"; do
+    if [[ ${args[i]} == --* ]]; then unset 'args[i]'; fi
+  done
 
-	echo:Common "${cl_grey}Filtered args:" "$@" "~>" "${args[*]}" "${cl_reset}" >&2
+  echo:Common "${cl_grey}Filtered args:" "$@" "~>" "${args[*]}" "${cl_reset}" >&2
 
-	# shellcheck disable=SC2207,SC2116
-	ARGS_NO_FLAGS=($(echo "${args[*]}"))
+  # shellcheck disable=SC2207,SC2116
+  ARGS_NO_FLAGS=($(echo "${args[*]}"))
 }
 
 if [ -z "$ARGS_DEFINITION" ]; then export ARGS_DEFINITION="-h,--help -v,--version=:1.0.0 --debug=DEBUG:*"; fi
 
 # Utility function, that extract output definition for parse:arguments function
 function parse:extract_output_definition() {
-	local definition=$1
+  local definition=$1
 
-	# extract output variable name, examples:
-	local name=${definition%%=*}
-	local name_as_value=${name//-/}
-	local output=$(echo "$2" | awk -v def="$definition" -F'=' '{ if ($2) {print $2} else {print def} }')
-	local variable=""
-	local default="1"
-	local args_qt="0"
+  # extract output variable name, examples:
+  local name=${definition%%=*}
+  local name_as_value=${name//-/}
+  local output=$(echo "$2" | awk -v def="$definition" -F'=' '{ if ($2) {print $2} else {print def} }')
+  local variable=""
+  local default="1"
+  local args_qt="0"
 
-	# extract variable name
-	if [[ "$output" == "$definition" ]]; then # simplest: --cookies
-		variable=$name_as_value
-	elif [[ "$output" == *:* ]]; then # extended: --cookies=first:*, --cookies=first:default:1, --cookies=::1, --cookies=:, --cookies=first:
-		local tmp=${output%%:*} && variable=${tmp:-"$name_as_value"}
-	else
-		variable=$output # extended: --cookies=first
-	fi
+  # extract variable name
+  if [[ "$output" == "$definition" ]]; then # simplest: --cookies
+    variable=$name_as_value
+  elif [[ "$output" == *:* ]]; then # extended: --cookies=first:*, --cookies=first:default:1, --cookies=::1, --cookies=:, --cookies=first:
+    local tmp=${output%%:*} && variable=${tmp:-"$name_as_value"}
+  else
+    variable=$output # extended: --cookies=first
+  fi
 
-	# extract default value
-	if [[ "$output" == *:* ]]; then default=${output#*:} && default=${default%:*}; fi
+  # extract default value
+  if [[ "$output" == *:* ]]; then default=${output#*:} && default=${default%:*}; fi
 
-	# extract arguments quantity
-	if [[ "$output" == *:*:* ]]; then args_qt=${output##*:}; fi
+  # extract arguments quantity
+  if [[ "$output" == *:*:* ]]; then args_qt=${output##*:}; fi
 
-	# indexed arguments should expect only one parameter/argument
-	if [[ "$args_qt" -gt 1 ]] && [[ "$1" == "\$"* ]]; then
-		echo "Warning. Indexed variable '$1' should not be used for multiple arguments." >&2
-	fi
-	echo:Common "${cl_grey}$1 '$2' ~> $variable|$default|$args_qt${cl_reset}" >&2
+  # indexed arguments should expect only one parameter/argument
+  if [[ "$args_qt" -gt 1 ]] && [[ "$1" == "\$"* ]]; then
+    echo "Warning. Indexed variable '$1' should not be used for multiple arguments." >&2
+  fi
+  echo:Common "${cl_grey}$1 '$2' ~> $variable|$default|$args_qt${cl_reset}" >&2
 
-	echo "$variable|$default|$args_qt"
+  echo "$variable|$default|$args_qt"
 }
 
 # parse ARGS_DEFINITION string to global arrays: lookup_arguments, index_to_outputs, index_to_args_qt, index_to_default
 function parse:mapping() {
-	local args=("$@")
+  local args=("$@")
 
-	# TODO (olku): trim whitespaces in $ARGS_DEFINITION, not spaces in begining or end, no double spaces
-	echo:Common "${cl_grey}Definition: $ARGS_DEFINITION${cl_reset}" >&2
+  # TODO (olku): trim whitespaces in $ARGS_DEFINITION, not spaces in begining or end, no double spaces
+  echo:Common "${cl_grey}Definition: $ARGS_DEFINITION${cl_reset}" >&2
 
-	# extract definition of each argument, separated by space, remove last empty element
-	readarray -td ' ' definitions <<<"$ARGS_DEFINITION " && unset 'definitions[-1]'
+  # extract definition of each argument, separated by space, remove last empty element
+  readarray -td ' ' definitions <<<"$ARGS_DEFINITION " && unset 'definitions[-1]'
 
-	# build lookup map of arguments, extract the longest name of each argument
-	declare -A -g lookup_arguments && lookup_arguments=() # key-to-index_of_definition. e.g. -c -> 0, --cookies -> 0
-	declare -A -g index_to_outputs && index_to_outputs=() # index-to-variable_name, e.g. -c,--cookies -> 0=cookies
-	declare -A -g index_to_args_qt && index_to_args_qt=() # index-to-argument_qauntity, e.g. -c,--cookies -> 0="0"
-	declare -A -g index_to_default && index_to_default=() # index-to-argument_default, e.g. -c,--cookies -> 0="", -c=:default:1 -> 0="default"
+  # build lookup map of arguments, extract the longest name of each argument
+  declare -A -g lookup_arguments && lookup_arguments=() # key-to-index_of_definition. e.g. -c -> 0, --cookies -> 0
+  declare -A -g index_to_outputs && index_to_outputs=() # index-to-variable_name, e.g. -c,--cookies -> 0=cookies
+  declare -A -g index_to_args_qt && index_to_args_qt=() # index-to-argument_qauntity, e.g. -c,--cookies -> 0="0"
+  declare -A -g index_to_default && index_to_default=() # index-to-argument_default, e.g. -c,--cookies -> 0="", -c=:default:1 -> 0="default"
 
-	# build parameters mapping
-	for i in "${!definitions[@]}"; do
-		# TODO (olku): validate the pattern format, otherwise throw an error
-		# shellcheck disable=SC2206
-		local keys=(${definitions[i]//,/ })
-		for key in "${keys[@]}"; do
-			local name=${key%%=*} # extract clean key name, e.g. --cookies=first -> --cookies
-			local helper=$(parse:extract_output_definition "$key" "${definitions[i]}")
+  # build parameters mapping
+  for i in "${!definitions[@]}"; do
+    # TODO (olku): validate the pattern format, otherwise throw an error
+    # shellcheck disable=SC2206
+    local keys=(${definitions[i]//,/ })
+    for key in "${keys[@]}"; do
+      local name=${key%%=*} # extract clean key name, e.g. --cookies=first -> --cookies
+      local helper=$(parse:extract_output_definition "$key" "${definitions[i]}")
 
-			# do the mapping
-			lookup_arguments[$name]=$i
-			index_to_outputs[$i]=$(echo "$helper" | awk -F'|' '{print $1}')
-			index_to_args_qt[$i]=$(echo "$helper" | awk -F'|' '{print $3}')
-			index_to_default[$i]=$(echo "$helper" | awk -F'|' '{print $2}')
-		done
-	done
+      # do the mapping
+      lookup_arguments[$name]=$i
+      index_to_outputs[$i]=$(echo "$helper" | awk -F'|' '{print $1}')
+      index_to_args_qt[$i]=$(echo "$helper" | awk -F'|' '{print $3}')
+      index_to_default[$i]=$(echo "$helper" | awk -F'|' '{print $2}')
+    done
+  done
 
 }
 
 # pattern: "{argument},-{short},--{alias}={output}:{init_value}:{args_quantity}"
 function parse:arguments() {
-	local args=("$@")
+  local args=("$@")
 
-	parse:mapping "$@"
+  parse:mapping "$@"
 
-	local index=1             # indexed input arguments without pre-flag
-	local skip_next_counter=0 # how many argument to skip from processing
-	local skip_aggregated=""  # all skipped arguments placed into one array
-	local last_processed=""   # last processed argument
-	local separator=""        # separator between aggregated arguments
+  local index=1             # indexed input arguments without pre-flag
+  local skip_next_counter=0 # how many argument to skip from processing
+  local skip_aggregated=""  # all skipped arguments placed into one array
+  local last_processed=""   # last processed argument
+  local separator=""        # separator between aggregated arguments
 
-	# parse the script arguments and resolve them to output variables
-	for i in "${!args[@]}"; do
-		local argument=${args[i]}
-		local value=""
-		local by_index="\$$index"
+  # parse the script arguments and resolve them to output variables
+  for i in "${!args[@]}"; do
+    local argument=${args[i]}
+    local value=""
+    local by_index="\$$index"
 
-		# extract key and value from argument, if used format `--key=value`
-		# shellcheck disable=SC2206
-		if [[ "$argument" == *=* ]]; then local tmp=(${argument//=/ }) && value=${tmp[1]:-"<empty>"} && argument=${tmp[0]}; fi
+    # extract key and value from argument, if used format `--key=value`
+    # shellcheck disable=SC2206
+    if [[ "$argument" == *=* ]]; then local tmp=(${argument//=/ }) && value=${tmp[1]:-"<empty>"} && argument=${tmp[0]}; fi
 
-		# accumulate arguments that reserved by last processed argument
-		if [ "$skip_next_counter" -gt 0 ]; then
-			skip_next_counter=$((skip_next_counter - 1))
-			skip_aggregated="${skip_aggregated}${separator}${argument}"
-			separator=" "
-			continue
-		fi
+    # accumulate arguments that reserved by last processed argument
+    if [ "$skip_next_counter" -gt 0 ]; then
+      skip_next_counter=$((skip_next_counter - 1))
+      skip_aggregated="${skip_aggregated}${separator}${argument}"
+      separator=" "
+      continue
+    fi
 
-		# if skipped aggregated var contains value assign it to the last processed argument
-		if [ ${#skip_aggregated} -gt 0 ]; then
-			local tmpValue="$skip_aggregated" && skip_aggregated="" && separator=""
+    # if skipped aggregated var contains value assign it to the last processed argument
+    if [ ${#skip_aggregated} -gt 0 ]; then
+      local tmpValue="$skip_aggregated" && skip_aggregated="" && separator=""
 
-			# assign aggregated value to output variable
-			local tmp_index=${lookup_arguments[$last_processed]}
-			echo:Common "[$LINENO] export ${index_to_outputs[$tmp_index]}=\"$tmpValue\""
-			eval "export ${index_to_outputs[$tmp_index]}=\"$tmpValue\""
-		fi
+      # assign aggregated value to output variable
+      local tmp_index=${lookup_arguments[$last_processed]}
+      echo:Common "[$LINENO] export ${index_to_outputs[$tmp_index]}='$tmpValue'"
+      eval "export ${index_to_outputs[$tmp_index]}='$tmpValue'"
+    fi
 
-		# process flags
-		if [ ${lookup_arguments[$argument]+_} ]; then
-			last_processed=$argument
-			local tmp_index=${lookup_arguments[$argument]}
-			local expected=${index_to_args_qt[$tmp_index]}
+    # process flags
+    if [ ${lookup_arguments[$argument]+_} ]; then
+      last_processed=$argument
+      local tmp_index=${lookup_arguments[$argument]}
+      local expected=${index_to_args_qt[$tmp_index]}
 
-			# assign default value to the output variable first
-			echo:Common "[$LINENO] export ${index_to_outputs[$tmp_index]}=\"${index_to_default[$tmp_index]}\""
-			eval "export ${index_to_outputs[$tmp_index]}=\"${index_to_default[$tmp_index]}\""
+      # assign default value to the output variable first
+      echo:Common "[$LINENO] export ${index_to_outputs[$tmp_index]}='${index_to_default[$tmp_index]}'"
+      eval "export ${index_to_outputs[$tmp_index]}='${index_to_default[$tmp_index]}'"
 
-			# if expected more arguments than provided, configure skip_next_counter
-			if [ "$expected" -gt 0 ]; then
-				skip_next_counter=$expected
-				skip_aggregated="$value" # assign current value to the skip_aggregated
-				if [ -n "$value" ]; then skip_next_counter=$((skip_next_counter - 1)) && separator=" "; fi
-				# TODO: we have DEFAULT value for the argument, but we can't use it, because we need
-				#   to check next argument, and if its a 'flag' apply DEFAULTS, otherwise use the
-				#   provided argument
-				continue
-			else
-				# default value is re-assigned by provided value
-				if [ -n "$value" ]; then
-					echo:Common "[$LINENO] export ${index_to_outputs[$tmp_index]}=\"$value\""
-					eval "export ${index_to_outputs[$tmp_index]}=\"$value\""
-				fi
-			fi
-		else
-			# process plain unnamed arguments
-			case $argument in
-			-*) echo:Common "${cl_grey}ignored: $argument ($value)${cl_reset}" >&2 ;;
-			*)
-				if [ ${lookup_arguments[$by_index]+_} ]; then
-					last_processed=$by_index
-					local tmp_index=${lookup_arguments[$by_index]}
+      # if expected more arguments than provided, configure skip_next_counter
+      if [ "$expected" -gt 0 ]; then
+        skip_next_counter=$expected
+        skip_aggregated="$value" # assign current value to the skip_aggregated
+        if [ -n "$value" ]; then skip_next_counter=$((skip_next_counter - 1)) && separator=" "; fi
+        # TODO: we have DEFAULT value for the argument, but we can't use it, because we need
+        #   to check next argument, and if its a 'flag' apply DEFAULTS, otherwise use the
+        #   provided argument
+        continue
+      else
+        # default value is re-assigned by provided value
+        if [ -n "$value" ]; then
+          echo:Common "[$LINENO] export ${index_to_outputs[$tmp_index]}='$value'"
+          eval "export ${index_to_outputs[$tmp_index]}='$value'"
+        fi
+      fi
+    else
+      # process plain unnamed arguments
+      case $argument in
+      -*) echo:Common "${cl_grey}ignored: $argument ($value)${cl_reset}" >&2 ;;
+      *)
+        if [ ${lookup_arguments[$by_index]+_} ]; then
+          last_processed=$by_index
+          local tmp_index=${lookup_arguments[$by_index]}
 
-					echo:Common "[$LINENO] export ${index_to_outputs[$tmp_index]}=\"$argument\""
-					eval "export ${index_to_outputs[$tmp_index]}=\"$argument\""
-				else
-					echo:Common "${cl_grey}ignored: $argument [$by_index] vs $last_processed:$skip_next_counter:$skip_aggregated:$value ${cl_reset}" >&2
-				fi
-				index=$((index + 1))
-				;;
-			esac
-		fi
-	done
+          echo:Common "[$LINENO] export ${index_to_outputs[$tmp_index]}='$argument'"
+          eval "export ${index_to_outputs[$tmp_index]}='$argument'"
+        else
+          echo:Common "${cl_grey}ignored: $argument [$by_index] vs $last_processed:$skip_next_counter:$skip_aggregated:$value ${cl_reset}" >&2
+        fi
+        index=$((index + 1))
+        ;;
+      esac
+    fi
+  done
 
-	if [ "$skip_next_counter" -gt 0 ]; then
-		echo "Error. Too little arguments provided"
-		exit 1
-	fi
+  if [ "$skip_next_counter" -gt 0 ]; then
+    echo "Error. Too little arguments provided"
+    exit 1
+  fi
 
-	# if aggregated var contains something
-	if [ ${#skip_aggregated} -gt 0 ]; then
-		local value="$skip_aggregated" && skip_aggregated="" && separator=""
-		local tmp_index=${lookup_arguments[$last_processed]}
-		echo:Common "[$LINENO] export ${index_to_outputs[$tmp_index]}=\"$value\""
-		eval "export ${index_to_outputs[$tmp_index]}=\"$value\""
-	fi
+  # if aggregated var contains something
+  if [ ${#skip_aggregated} -gt 0 ]; then
+    local value="$skip_aggregated" && skip_aggregated="" && separator=""
+    local tmp_index=${lookup_arguments[$last_processed]}
+    echo:Common "[$LINENO] export ${index_to_outputs[$tmp_index]}='$value'"
+    eval "export ${index_to_outputs[$tmp_index]}='$value'"
+  fi
 
-	# debug output
-	echo:Common "definition to output index:"
-	printf:Common '%s\n' "${!definitions[@]}" "${definitions[@]}" | pr -2t
-	echo:Common "'index', 'output variable name', 'args quantity', 'defaults':"
-	printf:Common '\"%s\"\n' "${!index_to_outputs[@]}" "${index_to_outputs[@]}" "${index_to_args_qt[@]}" "${index_to_default[@]}" | pr -4t | sort
-	for variable in "${index_to_outputs[@]}"; do
-		declare -n var_ref=$variable
-		echo:Common "${cl_grey}extracted: $variable=$var_ref${cl_reset}"
-	done
+  # debug output
+  echo:Common "definition to output index:"
+  printf:Common '%s\n' "${!definitions[@]}" "${definitions[@]}" | pr -2t
+  echo:Common "'index', 'output variable name', 'args quantity', 'defaults':"
+  printf:Common '\"%s\"\n' "${!index_to_outputs[@]}" "${index_to_outputs[@]}" "${index_to_args_qt[@]}" "${index_to_default[@]}" | pr -4t | sort
+  for variable in "${index_to_outputs[@]}"; do
+    declare -n var_ref=$variable
+    echo:Common "${cl_grey}extracted: $variable=$var_ref${cl_reset}"
+  done
 }
 
 # global associative array for flag-to-description mapping
@@ -217,23 +217,23 @@ if [ -z "$args_to_description" ]; then declare -A -g args_to_description && args
 if [ -z "$args_to_group" ]; then declare -A -g args_to_group && args_to_group=(); fi
 
 function parse:descr() {
-	local flag=$1
-	local description=$2
-	local group=${3:-"common"}
-	local order=${4:-100}
+  local flag=$1
+  local description=$2
+  local group=${3:-"common"}
+  local order=${4:-100}
 
-	args_to_description[flag]="${description}"
-	args_to_group[flag]="${group}"
+  args_to_description[flag]="${description}"
+  args_to_group[flag]="${group}"
 }
 
 # print help for ARGS_DEFINITION parameters
 function print:help() {
-	# if multiple groups defined in $args_to_group then print each group separately
-	if [ ${#args_to_group[@]} -gt 1 ]; then
-		: # TODO (olku): implement me
-	fi
+  # if multiple groups defined in $args_to_group then print each group separately
+  if [ ${#args_to_group[@]} -gt 1 ]; then
+    : # TODO (olku): implement me
+  fi
 
-	# print help for each argument
+  # print help for each argument
 }
 
 # This is the writing style presented by ShellSpec, which is short but unfamiliar.
--- .scripts/_dependencies.sh
+++ .scripts/_dependencies.sh
@@ -16,118 +16,118 @@ source "$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/_logger.sh"
 
 # shellcheck disable=SC2001,SC2155,SC2046,SC2116
 function isDebug() {
-	local args=("$@")
-	if [[ "${args[*]}" =~ "--debug" ]]; then echo true; else echo false; fi
+  local args=("$@")
+  if [[ "${args[*]}" =~ "--debug" ]]; then echo true; else echo false; fi
 }
 
 function isExec() {
-	local args=("$@")
-	if [[ "${args[*]}" =~ "--exec" ]]; then echo true; else echo false; fi
+  local args=("$@")
+  if [[ "${args[*]}" =~ "--exec" ]]; then echo true; else echo false; fi
 }
 
 function isOptional() {
-	local args=("$@")
-	if [[ "${args[*]}" =~ "--optional" ]]; then echo true; else echo false; fi
+  local args=("$@")
+  if [[ "${args[*]}" =~ "--optional" ]]; then echo true; else echo false; fi
 }
 
 function isSilent() {
-	local args=("$@")
-	if [[ "${args[*]}" =~ "--silent" ]]; then echo true; else echo false; fi
+  local args=("$@")
+  if [[ "${args[*]}" =~ "--silent" ]]; then echo true; else echo false; fi
 }
 
 # shellcheck disable=SC2001,SC2155,SC2086
 function dependency() {
-	local tool_name=$1
-	local tool_version_pattern=$2
-	local tool_fallback=${3:-"No details. Please google it."}
-	local tool_version_flag=${4:-"--version"}
-	local is_exec=$(isExec "$@")
-	local is_optional=$(isOptional "$@")
+  local tool_name=$1
+  local tool_version_pattern=$2
+  local tool_fallback=${3:-"No details. Please google it."}
+  local tool_version_flag=${4:-"--version"}
+  local is_exec=$(isExec "$@")
+  local is_optional=$(isOptional "$@")
 
-	config:logger:Dependencies "$@" # refresh debug flags
+  config:logger:Dependencies "$@" # refresh debug flags
 
-	# escape symbols: & / . { }, remove end of line, replace * by expectation from 1 to 4 digits
-	local tool_version=$(sed -e 's#[&\\/\.{}]#\\&#g; s#$#\\#' -e '$s#\\$##' -e 's#*#[0-9]\\{1,4\\}#g' <<<$tool_version_pattern)
+  # escape symbols: & / . { }, remove end of line, replace * by expectation from 1 to 4 digits
+  local tool_version=$(sed -e 's#[&\\/\.{}]#\\&#g; s#$#\\#' -e '$s#\\$##' -e 's#*#[0-9]\\{1,4\\}#g' <<<$tool_version_pattern)
 
-	# try to find tool
-	local which_tool=$(command -v $tool_name)
+  # try to find tool
+  local which_tool=$(command -v $tool_name)
 
-	if [ -z "$which_tool" ]; then
-		printf:Dependencies "which  : %s\npattern: %s, sed: \"s#.*\(%s\).*#\1#g\"\n-------\n" \
-			"${which_tool:-"command -v $tool_name"}" "$tool_version_pattern" "$tool_version"
+  if [ -z "$which_tool" ]; then
+    printf:Dependencies "which  : %s\npattern: %s, sed: \"s#.*\(%s\).*#\1#g\"\n-------\n" \
+      "${which_tool:-"command -v $tool_name"}" "$tool_version_pattern" "$tool_version"
 
-		if $is_optional; then
-			# shellcheck disable=SC2154
-			echo "Optional   [${cl_red}NO${cl_reset}]: \`$tool_name\` - ${cl_red}not found${cl_reset}! Try: ${cl_purple}$tool_fallback${cl_reset}"
-			return 0
-		else
-			echo "${cl_red}Error: dependency \`$tool_name\` not found."
-			echo "${cl_reset} Hint. To install tool use the command below: "
-			echo " \$>  $tool_fallback"
-			return 1
-		fi
-	fi
+    if $is_optional; then
+      # shellcheck disable=SC2154
+      echo "Optional   [${cl_red}NO${cl_reset}]: \`$tool_name\` - ${cl_red}not found${cl_reset}! Try: ${cl_purple}$tool_fallback${cl_reset}"
+      return 0
+    else
+      echo "${cl_red}Error: dependency \`$tool_name\` not found."
+      echo "${cl_reset} Hint. To install tool use the command below: "
+      echo " \$>  $tool_fallback"
+      return 1
+    fi
+  fi
 
-	local version_message=$($tool_name $tool_version_flag 2>&1)
-	local version_cleaned=$(echo "'$version_message'" | sed -n "s#.*\($tool_version\).*#\1#p" | head -1)
+  local version_message=$($tool_name $tool_version_flag 2>&1)
+  local version_cleaned=$(echo "'$version_message'" | sed -n "s#.*\($tool_version\).*#\1#p" | head -1)
 
-	printf:Dependencies "which  : %s\nversion: %s\npattern: %s, sed: \"s#.*\(%s\).*#\1#g\"\nver.   : %s\n-------\n" \
-		"$which_tool" "$version_message" "$tool_version_pattern" "$tool_version" "$version_cleaned"
+  printf:Dependencies "which  : %s\nversion: %s\npattern: %s, sed: \"s#.*\(%s\).*#\1#g\"\nver.   : %s\n-------\n" \
+    "$which_tool" "$version_message" "$tool_version_pattern" "$tool_version" "$version_cleaned"
 
-	if [ "$version_cleaned" == "" ]; then
-		if $is_optional; then
-			echo "Optional   [${cl_red}NO${cl_reset}]: \`$tool_name\` - ${cl_red}wrong version${cl_reset}! Try: ${cl_purple}$tool_fallback${cl_reset}"
-			return 0
-		else
-			echo "${cl_red}Error: dependency version \`$tool_name\` is wrong."
-			echo " Extracted: \`$version_cleaned\`"
-			echo " Expected : \`$tool_version_pattern\`${cl_reset}"
+  if [ "$version_cleaned" == "" ]; then
+    if $is_optional; then
+      echo "Optional   [${cl_red}NO${cl_reset}]: \`$tool_name\` - ${cl_red}wrong version${cl_reset}! Try: ${cl_purple}$tool_fallback${cl_reset}"
+      return 0
+    else
+      echo "${cl_red}Error: dependency version \`$tool_name\` is wrong."
+      echo " Extracted: \`$version_cleaned\`"
+      echo " Expected : \`$tool_version_pattern\`${cl_reset}"
 
-			if $is_exec; then
-				# shellcheck disable=SC2006,SC2154
-				echo " Executing: ${cl_yellow}${tool_fallback}${cl_reset}"
-				echo ""
-				eval $tool_fallback
-			else
-				echo ""
-				echo " Hint. To install tool use the command below: "
-				echo " \$>  $tool_fallback"
-				return 1
-			fi
-		fi
-	else
-		if $is_optional; then echo -n "Optional   "; else echo -n "Dependency "; fi
-		# shellcheck disable=SC2154
-		echo "[${cl_green}OK${cl_reset}]: \`$tool_name\` - version: $version_cleaned"
-	fi
+      if $is_exec; then
+        # shellcheck disable=SC2006,SC2154
+        echo " Executing: ${cl_yellow}${tool_fallback}${cl_reset}"
+        echo ""
+        eval $tool_fallback
+      else
+        echo ""
+        echo " Hint. To install tool use the command below: "
+        echo " \$>  $tool_fallback"
+        return 1
+      fi
+    fi
+  else
+    if $is_optional; then echo -n "Optional   "; else echo -n "Dependency "; fi
+    # shellcheck disable=SC2154
+    echo "[${cl_green}OK${cl_reset}]: \`$tool_name\` - version: $version_cleaned"
+  fi
 }
 
 function optional() {
-	local args=("$@")
+  local args=("$@")
 
-	# remove all flags from call
-	local del=("--debug" "--exec" "--silent" "--optional")
-	for value in "${del[@]}"; do
-		for i in "${!args[@]}"; do
-			if [[ ${args[i]} == "${value}" ]]; then unset 'args[i]'; fi
-		done
-	done
+  # remove all flags from call
+  local del=("--debug" "--exec" "--silent" "--optional")
+  for value in "${del[@]}"; do
+    for i in "${!args[@]}"; do
+      if [[ ${args[i]} == "${value}" ]]; then unset 'args[i]'; fi
+    done
+  done
 
-	# inject default parameters
-	if [ "${#args[@]}" == "2" ]; then
-		args+=("No details. Please google it." "--version")
-	elif [ "${#args[@]}" == "3" ]; then
-		args+=("--version")
-	fi
+  # inject default parameters
+  if [ "${#args[@]}" == "2" ]; then
+    args+=("No details. Please google it." "--version")
+  elif [ "${#args[@]}" == "3" ]; then
+    args+=("--version")
+  fi
 
-	# recover flags
-	if [ "$(isExec "$@")" == "true" ]; then args+=("--exec"); fi
-	if [ "$(isSilent "$@")" == "true" ]; then args+=("--silent"); fi
-	if [ "$(isDebug "$@")" == "true" ]; then args+=("--debug"); fi
-	args+=("--optional")
+  # recover flags
+  if [ "$(isExec "$@")" == "true" ]; then args+=("--exec"); fi
+  if [ "$(isSilent "$@")" == "true" ]; then args+=("--silent"); fi
+  if [ "$(isDebug "$@")" == "true" ]; then args+=("--debug"); fi
+  args+=("--optional")
 
-	# we should expand any number of input arguments to required 4 + extra flags
-	dependency "${args[@]}"
+  # we should expand any number of input arguments to required 4 + extra flags
+  dependency "${args[@]}"
 }
 
 # This is the writing style presented by ShellSpec, which is short but unfamiliar.
