bc13d6c45357f72f1b6f1345c2f3fdc8b48ac791
Oleksandr <oleksandr.kucherenko@artfulbits.se>
2023-11-24 00:54:17 +0100

    first version of profiler (#3)
    Co-authored-by: Oleksandr Kucherenko <o.kucherenko2@klarna.com>

---
+++ bin/profiler/profile.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+tm=$(date +%Y_%m_%d_%H_%M_%S)
+
+# execute tracing
+PS4='+ $(echo -n "$EPOCHREALTIME [$LINENO]: ")' bash -x "$@" 2>"trace.$tm.log" 1>/dev/null
+# finalize the tracing
+echo "+ $(gdate "+%s.%N") [0]: tracing done!" >>trace.log
+
+# publish the results, in file remove all colors
+"$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/tracing.sh" <"./trace.$tm.log" |
+    tee >(sed -r 's/\x1B\[(;?[0-9]{1,3})+[mGK]//g' >"./trace.$tm.final.log")
---
+++ bin/profiler/tracing.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# ++++ 1700771567.209177 [103]: prefix_sub_folder
+# ++++ 1700771567.210601 [87]: local tmpFileName=temp.file
+# ++++++ 1700771567.213742 [88]: monorepo_root
+# ++++++++ 1700771567.217496 [60]: dirname bin/version-up.sh
+
+gawk 'BEGIN {prev_time=0; first_line=1; line_num=0; depth=0; code=""} {
+    # Skip lines that do not start with `+`
+    if ($0 !~ /^\+/) {
+        # uncomment this line to print non-tracing lines
+        # print $0
+        next
+    }
+
+    # Extract timestamp and calculate delta time in ms
+    timestamp=($2+0) # force to number
+
+    if (first_line == 0) {
+        delta_time=(timestamp-prev_time)*1000
+        # is delta time in danger zone?
+        if (delta_time >= 10) { color=red } else { color=" " }
+        printf("%s%.3fms%s | [%04d] %*s%s\n", color, delta_time, reset, line_num, depth*2, "", code)
+    }
+
+    # colors
+    red="\033[31m"; reset="\033[0m";
+
+    # Count the depth
+    depth=gsub(/\+/, "&")
+
+    # Extract line number, and code (first 100 chars only)
+    split($3, arr, "[\\[\\]]")
+    line_num=arr[2]
+    code=substr($0, length($1 $2 $3)+3, 100)
+
+    # Calculate delta time in ms
+    if (first_line) { first_line=0 }   
+
+    # update prev_time for next line
+    prev_time=timestamp
+    delta_time=0
+} END {
+    # Print the last line, last command delta_time should be ZERO
+    printf(" %s%.3fms%s | [%04d] %*s%s\n", "\033[32m", delta_time, reset, line_num, depth*2, "", code)
+}'
