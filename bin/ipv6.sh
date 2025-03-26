5e99404a888128d6f33c8aae260b79e8fccf8731
Oleksandr Kucherenko <kucherenko.alex@gmail.com>
2025-03-26 13:27:19 +0100

    wip: cleanup

--- bin/ipv6.sh
+++ bin/ipv6.sh
@@ -8,6 +8,7 @@
 
 DEBUG=${DEBUG:-"-ipv6,-regex"}
 
+# shellcheck disable=SC2155
 [ -z "$E_BASH" ] && readonly E_BASH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && cd .. && pwd)"
 
 # shellcheck source=../.scripts/_colors.sh
@@ -31,7 +32,7 @@ function ipv6:grep() {
   local grep_regex="${ipv6_zone}|${ipv4_mapped}|${ipv4_mapped_alt}|${ipv6_full}|${ipv6_compressed}|${ipv6_loopback}"
 
   echo "${grep_regex}"
-  
+
   # debug output
   if type echo:Regex &>/dev/null; then
     echo:Regex "${grep_regex}" >&2
@@ -40,11 +41,11 @@ function ipv6:grep() {
 
 # ref1: https://regex101.com/r/uwPxJf/10
 function color:ipv6() {
-  local line=$1
+  local line=$1 grep_regex seeds
   local ipv6s ipv4s seeds
 
   # Combine all IPv6 patterns
-  local grep_regex=$(ipv6:grep)
+  grep_regex=$(ipv6:grep)
   local sed_regex="${grep_regex}"
 
   # Extract IPv6 addresses
@@ -52,14 +53,14 @@ function color:ipv6() {
   ipv4s=$(echo "$line" | ggrep -oPI '([0-9]{1,3}\.){3}[0-9]{1,3}') # IPv4 addresses format
 
   echo:Ipv6 "${cl_gray}$line${cl_reset}"
-  
+
   # Break down regex into components for readability
   local marker="\{IPv6\}"
   local sed_replace="s/(${sed_regex})/${cl_lgreen}${marker}${cl_reset}/Ig"
-  
+
   # Combine all patterns into a single regex
-  local seeds=$(echo "$line" | gsed -E "$sed_replace")
-  
+  seeds=$(echo "$line" | gsed -E "$sed_replace")
+
   # Use the combined regex in the sed command
   echo:Ipv6 "$seeds"
   echo:Ipv6 "ipv6: ${cl_yellow}${ipv6s/$'\n'/ }${cl_reset}"
@@ -82,25 +83,25 @@ function ipv6:compress() {
 
 # convert ipv6 from `2001:db8::1` to `2001:0db8:0000:0000:0000:0000:0000:0001` form.
 function ipv6:expand() {
-  local line="$1" final
-  local grep_regex=$(ipv6:grep)
+  local line="$1" final grep_regex sections replacer
+  grep_regex=$(ipv6:grep)
   local sub=":0000"
 
-  # count how many ':' we have in $line, excluding '::'. 
+  # count how many ':' we have in $line, excluding '::'.
   # example: 2001:db8::1 --> 3 (sub-sections is available)
   # example: 20::1 --> 2 (sub-sections is available)
   # example: 2001:db8::8d3:0:0:0 --> 6 (sub-sections is available)
-  local sections=$(echo "$line" | gsed -E 's/::/:/g' | tr -cd ':' | wc -c)
+  sections=$(echo "$line" | gsed -E 's/::/:/g' | tr -cd ':' | wc -c)
   local missing=$((7 - sections))
   echo:Ipv6 "sections: $((sections + 1)), missing: $missing"
 
   # start from 7 sub-sections and go down to 2, until we catch that grep returns a captured value
-  local replacer="$(printf "%0.s$sub" $(seq 1 $missing)):"
+  replacer="$(printf "%0.s$sub" $(seq 1 $missing)):"
   local sed_replace="s/::/${replacer}/g"
   final=$(echo "$line" | gsed -E "$sed_replace" | ggrep -oPI "${grep_regex}")
 
   # now its time to add leading zeros to each sub-section of ipv6
-  IFS=':' read -ra parts <<< "$final" && unset IFS
+  IFS=':' read -ra parts <<<"$final" && unset IFS
   final=$(printf '0000%s\n' "${parts[@]}" | gsed -E 's/.*([0-9a-f]{4}$)/\1/gi' | tr '\n' ':' | gsed 's/:$//g')
   echo "$final"
 }
@@ -109,5 +110,5 @@ function ipv6:expand() {
 # Note that it returns the current exit status (could be non-zero).
 ${__SOURCED__:+return}
 
-logger regex "$@" # declare echo:Regex & printf:Regex functions
+logger regex "$@"           # declare echo:Regex & printf:Regex functions
 logger:redirect regex ">&2" # redirect regex to STDERR
